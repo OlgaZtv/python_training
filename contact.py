063c1434c7751d46ea70866b4a1f7cd5e599bb93
OlgaZtv <zatulivetrova@gmail.com>
2021-02-08 12:58:20 +0300

    Выделили класс contact и переименовали переменные

--- contact.py
+++ contact.py
@@ -1,6 +1,6 @@
 class Contact:
 
-    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, email, email2, email3, homepage, adress2, phone2, notes):
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, adress2, phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -16,6 +16,12 @@ class Contact:
         self.email2 = email2
         self.email3 = email3
         self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
         self.adress2 = adress2
         self.phone2 = phone2
         self.notes = notes
