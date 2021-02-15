4e5aaca8b6c3657b65d54510dcc65138b5ee0077
OlgaZtv <zatulivetrova@gmail.com>
2021-02-15 17:17:54 +0300

    Создана общая фикстура

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application2 import ApplicationContact
 
-@pytest.fixture
-def app(request):
-    fixture = ApplicationContact()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
