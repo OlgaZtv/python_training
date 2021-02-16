1156bd9ec4ec7d16b6dce375235ad7cd9c094a24
OlgaZtv <zatulivetrova@gmail.com>
2021-02-16 11:11:18 +0300

    Создали тест на редактирование группы

---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="er4444", header="11122", footer="fffg"))
+    app.session.logout()
