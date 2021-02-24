bf8442ea346deef6f5ae9aa23a45b6fc84384276
OlgaZtv <zatulivetrova@gmail.com>
2021-02-24 12:32:52 +0300

    Реализована валидация фикстуры перед каждым тестом Доработали вспомогательный класс, реализующий операции login и logout (SessionHelper) -- добавили проверку текущего состояния сессии и выполнили действия с учётом текущего состояния

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,6 +2,4 @@ from model.contact import Contact
 
 
 def test_edit_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit(Contact(firstname="Vanya", middlename="Sergeevich", lastname="Sergeev", nickname="Vanya", title="Vanyaya", company="Ltd Vanya", address="Saint-Petersburg, Nevskyi 45", home="+78124555555", mobile="+76665554433", work="881266677888", fax="881266677766", email="ivanov@mail.ru", email2="vanya_work@mail.ru", email3="vanya_prevate@mail.ru", homepage="vanya.ya.ru", bday="5", bmonth="May", byear="1983", aday="4", amonth="August", ayear="2005", adress2="Saint-Petersburg, Zanevsky 15", phone2="Magnitogorskaya, 40", notes="Anna`s friend"))
-    app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,6 +2,5 @@
 from model.group import Group
 
 def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="er4444", header="11122", footer="fffg"))
-    app.session.logout()
+
