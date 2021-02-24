93bafcd278b0c32351e872c35a32f89a23812496
OlgaZtv <zatulivetrova@gmail.com>
2021-02-24 12:32:52 +0300

    Реализована валидациюя фикстуры перед каждым тестом Доработали вспомогательный класс, реализующий операции login и logout (SessionHelper) -- добавили проверку текущего состояния сессии и выполнили действия с учётом текущего состояния

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IvanIvan", title="Ivanushka", company="Ltd Sokol", address="Saint-Petersburg, Nevskyi 21", home="+781266677799", mobile="+76665554433", work="881266677888", fax="881266677766", email="ivanov@mail.ru", email2="ivanov_work@mail.ru", email3="Ivanon_prevate@mail.ru", homepage="ivanov.ya.ru", bday="9", bmonth="April", byear="1972", aday="2", amonth="May", ayear="2002", adress2="Saint-Petersburg, Zanevsky 5", phone2="Magnitogorskaya, 25", notes="Natasha`s friend"))
-    app.session.logout()
+
 
