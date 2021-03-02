50ab2eacb4172811d88ada35c0e47e1a9b990f10
OlgaZtv <zatulivetrova@gmail.com>
2021-03-02 13:51:44 +0300

    Модифицировали удаление и редактирование групп так, чтобы выбирался случайный элемент

--- fixture/group.py
+++ fixture/group.py
@@ -21,13 +21,21 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.group_cache = None
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
 
     def select_first_group(self):
         wd = self.app.wd
@@ -46,18 +54,21 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def modify_first_group(self, new_group_data):
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
-        #open modification form
+        self.select_group_by_index(index)
+        # open modification form
         wd.find_element_by_name("edit").click()
-        #fill group form
+        # fill group form
         self.fill(new_group_data)
-        #submit modification
+        # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
 
     def open_groups_page(self):
         wd = self.app.wd
