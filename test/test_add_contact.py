8984f52dfcbd9ff85117d3fe6db67cccf1166f99
OlgaZtv <zatulivetrova@gmail.com>
2021-03-05 16:45:07 +0300

    Добавили тесты для проверки телефонов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,11 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    old_contact_list = app.contact.get_contact_list()
+    old_contact_list = app.contact.get_contact_list
     contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IvanIvan", title="Ivanushka", company="Ltd Sokol", address="Saint-Petersburg, Nevskyi 21", home="+781266677799", mobile="+76665554433", work="881266677888", fax="881266677766", email="ivanov@mail.ru", email2="ivanov_work@mail.ru", email3="Ivanon_prevate@mail.ru", homepage="ivanov.ya.ru", bday="9", bmonth="April", byear="1972", aday="2", amonth="May", ayear="2002", address2="Saint-Petersburg, Zanevsky 5", phone2="Magnitogorskaya, 25", notes="Natasha`s friend")
     app.contact.add_contact(contact)
     assert len(old_contact_list) + 1 == app.contact.count()
-    new_contact = app.contact.get_contact_list()
+    new_contact = app.contact.get_contact_list
     old_contact_list.append(contact)
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
 
--- test/test_edit_contact_by_index.py
+++ test/test_edit_contact_by_index.py
@@ -6,12 +6,12 @@ def test_edit_contact(app):
         app.contact.create(Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test",
                      address="testtest", home="1235", mobile="12345", email="test", bday="5", bmonth="March",
                      byear="1980", address2="test", phone2="45657", notes="test"))
-    old_contact_list = app.contact.get_contact_list()
+    old_contact_list = app.contact.get_contact_list
     index = randrange(len(old_contact_list))
     contact = Contact(firstname="Vanya", lastname="Sergeev")
     contact.id = old_contact_list[index].id
     app.contact.modify_contact_by_index(index, contact)
-    new_contact = app.contact.get_contact_list()
+    new_contact = app.contact.get_contact_list
     assert len(old_contact_list) == len(new_contact)
     old_contact_list[index] = contact
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
