f0ab9fe7b6be1c7d3ea0ea30c090fb40a41b6394
OlgaZtv <zatulivetrova@gmail.com>
2021-03-05 16:05:53 +0300

    Добавили два метода: для редактирования контакта, для открытия формы детальной информации о контакте

--- fixture/contact.py
+++ fixture/contact.py
@@ -119,3 +119,17 @@ class ContactHelper:
                 contact_id = element.find_element_by_name("selected[]").get_attribute("value")
                 self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=contact_id))
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
